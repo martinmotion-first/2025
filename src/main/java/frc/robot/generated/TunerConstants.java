e6ccaa7a7db3860be065aff0d699da903d93ee13
Brian Layman <brian.s.layman@gmail.com>
2025-03-16 11:47:47 -0500

    More tweaks for teleop

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -30,7 +30,7 @@ public class TunerConstants {
     // When using closed-loop control, the drive motor uses the control
     // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
     private static final Slot0Configs driveGains = new Slot0Configs()
-        .withKP(1).withKI(0).withKD(0)
+        .withKP(.1).withKI(0).withKD(0)
         .withKS(0).withKV(0.124);
 
     // The closed-loop output type to use for the steer motors;
@@ -75,7 +75,7 @@ public class TunerConstants {
     // Theoretical free speed (m/s) at 12 V applied output;
     // This needs to be tuned to your individual robot
     //public static final LinearVelocity kSpeedAt12Volts = MetersPerSecond.of(4.73);
-    public static final LinearVelocity kSpeedAt12Volts = MetersPerSecond.of(4.73); 
+    public static final LinearVelocity kSpeedAt12Volts = MetersPerSecond.of(4); 
 
     // Every 1 rotation of the azimuth results in kCoupleRatio drive motor turns;
     // This may need to be tuned to your individual robot
