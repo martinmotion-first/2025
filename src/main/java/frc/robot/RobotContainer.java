0544ce31d61c45695ab09796b2c3549c28b63c69
Brian Layman <brian.s.layman@gmail.com>
2025-03-16 10:36:05 -0500

    Pared down autos. Crazy movement in simulator

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -90,8 +90,8 @@ public class RobotContainer {
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // autoChooser = AutoBuilder.buildAutoChooser("1dot5m straight with 90degree rotation");
-    autoChooser = AutoBuilder.buildAutoChooser("");
-    // SmartDashboard.putData("Auto Mode", autoChooser);
+    autoChooser = AutoBuilder.buildAutoChooser("Outside stay clear");
+    SmartDashboard.putData("Auto Mode", autoChooser);
     // SmartDashboard.putData("pigeon2", drivetrain.getPigeon2());
     // SmartDashboard.putNumber("drivetrain module 0 (front-left) encoder", drivetrain.getModule(0).getEncoder().getAbsolutePosition().getValueAsDouble());
     // SmartDashboard.putNumber("drivetrain module 1 (front-right) encoder", drivetrain.getModule(1).getEncoder().getAbsolutePosition().getValueAsDouble());
@@ -120,18 +120,22 @@ public class RobotContainer {
     // Configure the trigger bindings
 
     // NamedCommands.registerCommand("prepareCoralScoreCommand", RobotCommands.prepareCoralScoreCommand(null, elevator, arm, coralSim));
-    NamedCommands.registerCommand("autoPrepareCoralScoreCommandL2", RobotCommands.autoPrepareCoralScoreCommand(ScoreLevel.L2, elevator, arm, coralSim));
-    NamedCommands.registerCommand("autoPrepareCoralScoreCommandL3", RobotCommands.autoPrepareCoralScoreCommand(ScoreLevel.L3, elevator, arm, coralSim));
-    NamedCommands.registerCommand("autoPrepareCoralScoreCommandL4", RobotCommands.autoPrepareCoralScoreCommand(ScoreLevel.L4, elevator, arm, coralSim));
-    NamedCommands.registerCommand("scoreCoralCommand", RobotCommands.scoreCoralCommand(drivetrain, elevator, arm, coralSim));
-    NamedCommands.registerCommand("prepareIntakeCoralCommand", RobotCommands.prepareIntakeCoralCommand(elevator, arm, coralSim));
-    NamedCommands.registerCommand("intakeCoralCommand", RobotCommands.intakeCoralCommand(elevator, arm, coralSim));
-    NamedCommands.registerCommand("intakeIntoScoreCommandL2", RobotCommands.intakeIntoScoreCommand(ScoreLevel.L2, elevator, arm, coralSim));
-    NamedCommands.registerCommand("intakeIntoScoreCommandL3", RobotCommands.intakeIntoScoreCommand(ScoreLevel.L3, elevator, arm, coralSim));
-    NamedCommands.registerCommand("intakeIntoScoreCommandL4", RobotCommands.intakeIntoScoreCommand(ScoreLevel.L4, elevator, arm, coralSim));
-    NamedCommands.registerCommand("prepareAlgaeL2RemoveCommand", RobotCommands.prepareAlgaeL2RemoveCommand(elevator, arm));
-    NamedCommands.registerCommand("prepareAlgaeL3RemoveCommand", RobotCommands.prepareAlgaeL3RemoveCommand(elevator, arm));
-    NamedCommands.registerCommand("algaeRemoveCommand", RobotCommands.algaeRemoveCommand(elevator, arm));
+    NamedCommands.registerCommand("moveElevatorToArmFree", RobotCommands.elevatorOnlyMoveToPosition(elevator, Constants.Elevator.ElevatorPosition.ARM_FREE));
+    NamedCommands.registerCommand("prepareCoralL2", RobotCommands.elevatorCombinedCommand(elevator, arm, Constants.Elevator.ElevatorPosition.L2));
+    NamedCommands.registerCommand("prepareCoralL3", RobotCommands.elevatorCombinedCommand(elevator, arm, Constants.Elevator.ElevatorPosition.L3));
+    NamedCommands.registerCommand("scoreCoral", RobotCommands.scoreCoralCommand(drivetrain, elevator, arm, coralSim));
+    // NamedCommands.registerCommand("autoPrepareCoralScoreCommandL2", RobotCommands.autoPrepareCoralScoreCommand(ScoreLevel.L2, elevator, arm, coralSim));
+    // NamedCommands.registerCommand("autoPrepareCoralScoreCommandL3", RobotCommands.autoPrepareCoralScoreCommand(ScoreLevel.L3, elevator, arm, coralSim));
+    // NamedCommands.registerCommand("autoPrepareCoralScoreCommandL4", RobotCommands.autoPrepareCoralScoreCommand(ScoreLevel.L4, elevator, arm, coralSim));
+    // NamedCommands.registerCommand("scoreCoralCommand", RobotCommands.scoreCoralCommand(drivetrain, elevator, arm, coralSim));
+    // NamedCommands.registerCommand("prepareIntakeCoralCommand", RobotCommands.prepareIntakeCoralCommand(elevator, arm, coralSim));
+    // NamedCommands.registerCommand("intakeCoralCommand", RobotCommands.intakeCoralCommand(elevator, arm, coralSim));
+    // NamedCommands.registerCommand("intakeIntoScoreCommandL2", RobotCommands.intakeIntoScoreCommand(ScoreLevel.L2, elevator, arm, coralSim));
+    // NamedCommands.registerCommand("intakeIntoScoreCommandL3", RobotCommands.intakeIntoScoreCommand(ScoreLevel.L3, elevator, arm, coralSim));
+    // NamedCommands.registerCommand("intakeIntoScoreCommandL4", RobotCommands.intakeIntoScoreCommand(ScoreLevel.L4, elevator, arm, coralSim));
+    // NamedCommands.registerCommand("prepareAlgaeL2RemoveCommand", RobotCommands.prepareAlgaeL2RemoveCommand(elevator, arm));
+    // NamedCommands.registerCommand("prepareAlgaeL3RemoveCommand", RobotCommands.prepareAlgaeL3RemoveCommand(elevator, arm));
+    // NamedCommands.registerCommand("algaeRemoveCommand", RobotCommands.algaeRemoveCommand(elevator, arm));
 
     configureButtonBindings();
   }
